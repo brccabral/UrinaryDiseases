6fd0631aa2fea539573061eb384e3fe5868779a7
Bruno Cabral <cabral.brc@gmail.com>
2022-02-15 20:26:21 -0800

    train and evaluate LinearModelDisease

--- model/model.py
+++ model/model.py
@@ -3,18 +3,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Linear1H(nn.Module):
+class LinearModelDisease(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, output_dim):
         super().__init__()
 
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
         self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim//2)
+        self.fc3 = nn.Linear(hidden_dim//2, output_dim)
         self.dropout = nn.Dropout(0.3)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-        self.norm = nn.Sigmoid()
 
     def forward(self, x):
-        output = F.relu(self.fc1(x))
-        output = self.dropout(output)
-        output = self.fc2(output)
-        return self.norm(output)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+        return F.log_softmax(x.view(-1, self.output_dim), dim=1)
