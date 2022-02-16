13eea19a9a4909ed09fe520d78601760962e24c2
Bruno Cabral <cabral.brc@gmail.com>
2022-02-16 11:45:53 -0800

    save model

--- model/model.py
+++ model/model.py
@@ -13,12 +13,12 @@ class LinearModelDisease(nn.Module):
         self.output_dim = output_dim
 
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim//2)
-        self.fc3 = nn.Linear(hidden_dim//2, output_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
         self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.dropout(F.relu(self.fc2(x)))
-        x = self.dropout(F.relu(self.fc3(x)))
+        x = F.relu(self.fc3(x))
         return F.log_softmax(x.view(-1, self.output_dim), dim=1)
